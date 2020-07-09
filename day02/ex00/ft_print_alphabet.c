0049d1006e7f5824f6f9e2a9552e4f942e8e8d11
lmudauWTC <68005578+lmudauWTC@users.noreply.github.com>
2020-07-09 17:22:50 +0200

    Update ft_print_alphabet.c

--- day02/ex00/ft_print_alphabet.c
+++ day02/ex00/ft_print_alphabet.c
@@ -1,3 +1,5 @@
+
+/* ************************************************************************** */
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -5,11 +7,40 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lmudau <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/07/09 13:33:07 by lmudau            #+#    #+#             */
-/*   Updated: 2020/07/09 13:41:39 by lmudau           ###   ########.fr       */
+/*   Created: 2020/07/09 16:16:27 by lmudau            #+#    #+#             */
+/*   Updated: 2020/07/09 16:16:27 by lmudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main ()
+#include <unistd.h>
+
+void ft_putchar(char c);
+
+void ft_print_alphabet(void)
 {
+	char start;
+	char end ;
+
+	start = 'a';
+	end = 'z'; 
+	while (start <= end)
+	{
+		ft_putchar(start);
+		start++;
+	}
+
+}
+
+void ft_putchar(char c)
+{
+	write (1, &c, 1); 
+}
+
+int main(void)
+{
+	ft_print_alphabet();
 	return (0);
+}
+
+	
+
